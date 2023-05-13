91a8298b5789167c3f8e6cb9393cbc4ba30c206d
sedasenbol <ssenbol@gmail.com>
2023-05-13 18:30:49 +0300

    DiceRoller class was added.

---
+++ second_flutter_app/lib/dice_roller.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+var currentDice = 1;
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  int next(int min, int max) => min + Random().nextInt(max - min);
+
+  void rollDice() {
+    setState(() {
+      currentDice = next(1, 6);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisSize: MainAxisSize.min, children: [
+      Image.asset(
+        'assets/images/dice-$currentDice.png',
+        width: 200,
+      ), //pixels
+      const SizedBox(
+        height: 20,
+      ),
+      TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+              foregroundColor: Colors.amber,
+              textStyle: const TextStyle(fontSize: 28)),
+          child: const Text("Roll Dice")),
+    ]);
+  }
+}
--- second_flutter_app/lib/gradient_container.dart
+++ second_flutter_app/lib/gradient_container.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:basics/styled_text.dart';
+import 'package:basics/dice_roller.dart';
 
 const beginAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -7,10 +7,11 @@ const endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors, {super.key});
 
-GradientContainer.purple({super.key}):colors = [
-            const Color.fromARGB(255, 83, 26, 216),
-            const Color.fromARGB(255, 161, 121, 218),
-          ];
+  GradientContainer.purple({super.key})
+      : colors = [
+          const Color.fromARGB(255, 83, 26, 216),
+          const Color.fromARGB(255, 161, 121, 218),
+        ];
 
   final List<Color> colors;
 
@@ -25,7 +26,7 @@ GradientContainer.purple({super.key}):colors = [
         ),
       ),
       child: Center(
-         child: Image.asset('assets/images/dice-2.png', width: 200), //pixels
+        child: DiceRoller(),
       ),
     );
   }
