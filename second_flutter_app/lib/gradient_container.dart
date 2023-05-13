17ecfc8e41542b6bad9a63ceb93546895d7c96b8
sedasenbol <ssenbol@gmail.com>
2023-05-13 13:10:16 +0300

    first dice image implementation.

--- second_flutter_app/lib/gradient_container.dart
+++ second_flutter_app/lib/gradient_container.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:basics/styled_text.dart';
+//import 'package:basics/styled_text.dart';
 
 const beginAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors, {super.key});
+
+GradientContainer.purple({super.key}):colors = [
+            const Color.fromARGB(255, 83, 26, 216),
+            const Color.fromARGB(255, 161, 121, 218),
+          ];
 
   final List<Color> colors;
 
@@ -19,7 +24,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center( child: StyledText('Hello world'),
+      child: Center(
+         child: Image.asset('assets/images/dice-2.png', width: 200), //pixels
       ),
     );
   }
